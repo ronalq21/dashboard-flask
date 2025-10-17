e1c8b1b308b85f2d030a3914bdd3d7175705c725
ronalq21 <ronaldhinojinezincacutip@gmail.com>
2025-10-16 19:12:31 -0500

    Corrijoooo ruta de carpeta codigos para Render

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, request, redirect, jsonify
-import os, subprocess
+import os, subprocess, sys
 
 app = Flask(__name__)
 
-RUTA_CODIGOS = os.path.join('codigos')
+# Ruta absoluta de la carpeta codigos
+RUTA_CODIGOS = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'codigos')
 
 @app.route('/')
 def inicio():
     datos_usuario = {
         'nombre': 'Ronaldhino',
         'apellidos': 'Jinez Incacutipa',
-        'carrera': ' Ingenieria Estadística e Informática',
+        'carrera': 'Ingenieria Estadística e Informática',
         'universidad': 'Universidad del Altiplano Puno',
         'año_ingreso': 2023,
         'foto': 'img/foto.jpg'
@@ -29,10 +30,11 @@ def upload():
 def run(nombre):
     ruta = os.path.join(RUTA_CODIGOS, nombre)
     try:
-        salida = subprocess.check_output(['python', ruta], text=True)
+        salida = subprocess.check_output([sys.executable, ruta], text=True)
     except subprocess.CalledProcessError as e:
         salida = e.output or str(e)
     return jsonify({"salida": salida})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
